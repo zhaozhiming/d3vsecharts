78b07f27dd251ca5cfbc433c684ebca17fbf8641
zhaozhiming <zhaozhiming003@gmail.com>
2017-09-12 23:43:16 +0800

    style: add color vars

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Home from './components/Home';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Home />
-    );
-  }
-}
-
-export default App;
+export default () => <Home />;
